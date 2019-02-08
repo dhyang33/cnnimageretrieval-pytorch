78298412e01dfe4a1620efe4fcbaeec57f1f1d7d
Evan Hubinger <evanjhub@gmail.com>
2019-02-07 20:46:55 -0800

    Improve API

--- benchmarks.py
+++ benchmarks.py
@@ -4,9 +4,16 @@ import torch
 import numpy as np
 from torch.utils.model_zoo import load_url
 from torchvision import transforms
+from torch.autograd import Variable
 
 from cirtorch.utils.general import get_data_root
-from cirtorch.networks.imageretrievalnet import init_network, extract_vectors
+from cirtorch.networks.imageretrievalnet import (
+    init_network,
+    extract_vectors,
+    extract_ss,
+    extract_ms,
+)
+from cirtorch.datasets.genericdataset import ImagesFromDataList
 from cirtorch.examples.test import (
     load_network,
     load_offtheshelf,
@@ -15,16 +22,60 @@ from cirtorch.examples.test import (
 from score_retrieval.data import indices_with_label
 
 
+def vectors_from_images(net, images, transform, ms=[1], msp=1, print_freq=10):
+    """Extract vectors from images given as a pytorch array."""
+    # convert images to pytorch
+    proc_images = []
+    for image in images:
+        proc_images.append(torch.from_numpy(image))
+
+    # moving network to gpu and eval mode
+    net.cuda()
+    net.eval()
+
+    # creating dataset loader
+    loader = torch.utils.data.DataLoader(
+        ImagesFromDataList(images=proc_images, transform=transform),
+        batch_size=1, shuffle=False, num_workers=8, pin_memory=True
+    )
+
+    # extracting vectors
+    vecs = torch.zeros(net.meta['outputdim'], len(proc_images))
+    for i, input in enumerate(loader):
+        input_var = Variable(input.cuda())
+
+        if len(ms) == 1:
+            vecs[:, i] = extract_ss(net, input_var)
+        else:
+            vecs[:, i] = extract_ms(net, input_var, ms, msp)
+
+        if (i+1) % print_freq == 0 or (i+1) == len(proc_images):
+            print('\r>>>> {}/{} done...'.format((i+1), len(proc_images)), end='')
+    print('')
+    return vecs
+
+
 def call_benchmark(
-    database_images,
-    database_labels,
-    query_images,
-    query_labels,
     network,
+    database_labels,
+    query_labels,
+
+    # must pass either database_images or database_paths but not both
+    database_images=None,
+    database_paths=None,
+
+    # must pass either query_images or query_paths but not both
+    query_images=None,
+    query_paths=None,
+
     offtheshelf=True,
-    image_size=1024
+    image_size=1024,
 ):
     """Run the given network on the given data and return ranks, gnd."""
+    # convert images
+    database_images
+
+    # load network
     if offtheshelf:
         net = load_offtheshelf(network)
     else:
@@ -52,9 +103,16 @@ def call_benchmark(
     gnd = [{"ok": indices_with_label(label, database_labels), "junk": []} for label in query_labels]
 
     print('>> processing database images...')
-    vecs = extract_vectors(net, database_images, image_size, transform, ms=ms, msp=msp)
+    if database_images is not None:
+        vecs = vectors_from_images(net, database_images, transform, ms=ms, msp=msp)
+    else:
+        vecs = extract_vectors(net, database_paths, image_size, transform, ms=ms, msp=msp)
+
     print('>> processing query images...')
-    qvecs = extract_vectors(net, query_images, image_size, transform, ms=ms, msp=msp)
+    if query_images is not None:
+        qvecs = vectors_from_images(net, query_images, transform, ms=ms, msp=msp)
+    else:
+        qvecs = extract_vectors(net, query_paths, image_size, transform, ms=ms, msp=msp)
 
     # convert to numpy
     vecs = vecs.numpy()
